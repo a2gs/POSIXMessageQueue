e347340755a1250dc0e3a807e9acbd865050bcc8
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2019-02-04 21:53:51 -0200

    Update comum.h

--- comum.h
+++ comum.h
@@ -1,5 +1,27 @@
+/* Andre Augusto Giannotti Scota (a2gs)                              
+ * andre.scota@gmail.com
+ *
+ * A POSIX Message Queue sample
+ *
+ * Public Domain
+ *
+ */
+
+/* <file name>
+ * <File description>
+ *
+ *  Who     | When       | What
+ *  --------+------------+----------------------------
+ *   a2gs   | 01/01/2005 | Creation
+ *          |            |
+ */
+
+#ifndef __PMQ_H__
+#define __PMQ_H__
 #define TEXTSIZE_SIZE 8
 #define TEXT_SIZE 100
 #define MSG_SIZE (TEXT_SIZE + TEXTSIZE_SIZE)
 
 #define QUEUE_FILE "/tmp/queuePosixMsg"
+
+#endif
